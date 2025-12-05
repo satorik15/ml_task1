48199786c86358085220b5047b3d1c7ed9fd2cc3
Александр Торяник <sashator15@yandex.ru>
2025-12-05 23:44:13 +0300

    fixed app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import plotly.express as px
-
+import sklearn
 
 MODEL_PATH = "best_Elastic_model.pkl"
 FEATURE_NAMES_PATH = "feature_info.pkl"
@@ -66,6 +66,7 @@ def prepare_features(df, feature_names,train_medians,scaler):
     df_proc = df.copy()
     #Выбор только числовых колонок
     numeric_columns=feature_names["feature_names"]
+    st.write(numeric_columns)
     df_proc=df_proc[numeric_columns]
     #Предобработка аналогично EDA
     #Удаление полных дубликатов
